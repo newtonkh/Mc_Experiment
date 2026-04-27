9007d53e673dd5efd96193ebb6fd48680e4be2a5
newtonkh <juug24bcas32748@jainuniversity.ac.in>
2026-04-27 20:59:54 +0530

    Create TableLayout.java

---
+++ Experiment-6/TableLayout.java
@@ -0,0 +1,12 @@
+package com.example.exp6;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TableLayout extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activtiy_table);
+    }
+}
