7b6ab7859aecbef7a29dbf989f8b71e3cf050960
newtonkh <juug24bcas32748@jainuniversity.ac.in>
2026-04-27 20:59:27 +0530

    Create RelativeLayout.java

---
+++ Experiment-6/RelativeLayout.java
@@ -0,0 +1,11 @@
+package com.example.exp6;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RelativeLayout extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_relative);
+    }
+}
